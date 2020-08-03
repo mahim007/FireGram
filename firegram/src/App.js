615700874d099dd65a63a21cdad63c972b5a8144
mahim007 <ashrafulmahim@gmail.com>
2020-08-03 16:25:09 +0600

    initial commit for firegram project

---
+++ firegram/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './comps/Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+    </div>
+    // kjfhdf sdfhksdfkj kjfskasfk skfhksdf ksdfksdfksd
+  );
+}
+
+export default App;
